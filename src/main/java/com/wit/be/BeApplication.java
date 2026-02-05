977d7a628b3e8709d49359b347481e2d419ae40b
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2026-02-05 19:53:40 +0900

    style: Spotless 코드 포맷팅 적용

--- src/main/java/com/wit/be/BeApplication.java
+++ src/main/java/com/wit/be/BeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BeApplication.class, args);
+    }
 }
